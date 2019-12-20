5abc3d53d408da4aba7c784c80db0e4028cf3f5f
a1016231744 <38521156+a1016231744@users.noreply.github.com>
2019-12-20 14:35:18 +0800

    Add files via upload

--- train_list.py
+++ train_list.py
@@ -5,27 +5,32 @@ import random
 
 
 #=============test800,之后根据修改number
-download_path = r'I:/xieyi/VehicleID_V1.0' # 数据集路径
-train_path = download_path + '/train_test_split/train_list.txt' #读取txt
-train_image_path = download_path + '/train_image'
+download_path = r'I:/All_data_tmp/VeRi776/VeRi'
+train_path = download_path + '/name_train.txt'
+train_image_path = 'E:/xieyi/EMDD_datasets/VeRi-776/train'
 
 f = open (train_path, 'r')
 lines = f.readlines()
 
-
 if not os.path.exists(train_image_path):
     os.mkdir(train_image_path)
 
+train_image_path = train_image_path + '/class_id'
+if not os.path.exists(train_image_path):
+    os.mkdir(train_image_path)
 
-for image_every in lines:
-    image_name = image_every.split(" ")[0]
-    image_id = image_every.split(" ")[1].strip('\n')
-    train_image_id_path = train_image_path + '/' + image_id
-    if not os.path.exists(train_image_id_path):
-        os.mkdir(train_image_id_path)
+for image_name in lines:
 
-    src_path = download_path + '/image/' + image_name + '.jpg'
-    dst_path = train_image_id_path + '/' + image_id + '_' + image_name + '.jpg'
+    image_name = image_name.strip("\n")
+    image_name_split  = image_name.split("_")
+    image_class = image_name_split[0]
+    print(image_class)
+    train_image_class_path = train_image_path + '/' + image_class
+    if not os.path.exists(train_image_class_path):
+        os.mkdir(train_image_class_path)
+
+    src_path = download_path + '/image_train/' + image_name
+    dst_path =  train_image_class_path + '/' + image_name
     shutil.copyfile(src_path, dst_path)
 
 
